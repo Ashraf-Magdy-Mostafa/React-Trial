baa2c3069e726ad220894a2b01517a83559a8247
Ashraf-Magdy-Mostafa <ashrafmagdy926@gmail.com>
2023-05-13 05:30:05 +0300

    added a filter by year for expenses

--- src/App.js
+++ src/App.js
@@ -1,43 +1,68 @@
 import { List } from "./newList";
 import { useState } from "react";
 import { NewExpense } from "./comp/NewExpense/NewExpense";
-import Expences from "./comp/expnces";
 import { Card } from "./comp/card";
+import { FilteredExpenses } from "./comp/filteredExpenses";
+import Expences from "./comp/expnces";
 export const DUMMY_EXPENSES = [
   {
     id: "e1",
     title: "Toilet Paper",
     amount: 94.12,
-    date: new Date(),
+    date: new Date(2022, 2, 12),
   },
   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
     title: "Car Insurance",
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2020, 2, 28),
   },
   {
     id: "e4",
     title: "New Desk (Wooden)",
     amount: 450,
-    date: new Date(),
+    date: new Date(2022, 2, 12),
   },
 ];
 function App() {
-  // const newArr = () => {};
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
   };
+  /* 
+    YEAR FILTER SECTION //*
+      */
+  const [year, setYear] = useState("2019");
+  function yearHandler(event) {
+    // sets the year based on selected value
+    console.log(event.target.value);
+    setYear(event.target.value);
+  }
+
+  const filteredExpensesArray = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === year; //2021
+    //return (filtered array where checks to be true the give condition above)
+  });
+
+  let expensesContent = filteredExpensesArray.map((expense) => (
+    <Expences
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+  ));
+
   return (
     <Card>
       <NewExpense onAddExpense={addExpenseHandler} />
       <h2>Let's get star2ted!</h2>
+      <FilteredExpenses selected={year} onChangeYear={yearHandler} />
 
-      <List onlist={expenses} />
+      <List onlist={filteredExpensesArray} />
     </Card>
   );
 }
